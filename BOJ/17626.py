b148b99980daf64e0b2b0dea3645bb70f90a5e69
nowimseeingcashflow <102157329+nowimseeingcashflow@users.noreply.github.com>
2025-01-31 16:27:09 +0900

    Add files via upload

---
+++ BOJ/17626.py
@@ -0,0 +1,16 @@
+# PyPy 필요, DP
+
+import math
+
+n = int(input())
+
+arr = [0, 1]
+
+for i in range(2, n + 1):
+    mint = 5
+    for j in range(1, int(math.sqrt(i)) + 1):
+        mint = min(mint, arr[i - j**2])
+
+    arr.append(mint + 1)
+    
+print(arr[n])
