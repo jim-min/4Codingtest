9ee250787c9fed6ca9f5930b7aa90435f2662e50
nowimseeingcashflow <102157329+nowimseeingcashflow@users.noreply.github.com>
2025-01-31 15:07:53 +0900

    Create 30979.py

---
+++ BOJ/30979.py
@@ -0,0 +1,9 @@
+care_time = int(input())
+candy_amount = int(input())
+candyz = list(map(int, input().split()))
+
+if sum(candyz) >= care_time :
+    print("Padaeng_i Happy")
+    
+else:
+    print("Padaeng_i Cry")
