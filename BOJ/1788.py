f2d6c22e051acfc7bc079c859f3d763a3ad0c324
jim-min <102157329+jim-min@users.noreply.github.com>
2025-03-13 15:24:06 +0900

    Add files via upload

---
+++ BOJ/1788.py
@@ -0,0 +1,20 @@
+put = int(input())
+
+MOD_BILLION = 1000000000;
+
+fibo = [0, 1]
+
+for i in range(0, 1000000):
+    fibo.append((fibo[i] + fibo[i + 1]) % MOD_BILLION)
+
+if (put == 0):
+    print(0)
+    print(0)
+
+else:
+    if (put < 0 and put % 2 == 0):
+        print(-1)
+    else:
+        print(1)
+    
+    print(fibo[abs(put)])
