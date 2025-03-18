2f55fbe00682d62981a598087e35fc8f34b8d75a
jim-min <102157329+jim-min@users.noreply.github.com>
2025-03-18 15:00:30 +0900

    Create 9663.py

---
+++ BOJ/9663.py
@@ -0,0 +1,30 @@
+n = int(input())
+total = 0
+
+queenRows = [0] * n
+
+def isPlacable(row, col):
+    if col in queenRows[0:row]:
+        return False
+    
+    for i in range(0, row):
+        if abs(row - i) == abs(col - queenRows[i]):
+            return False
+        
+    return True
+
+def nqueen(num):
+    if num == n:
+        global total
+        total += 1
+        return
+    
+    for i in range(n):
+        queenRows[num] = i
+        if not isPlacable(num, i):
+            continue
+        
+        nqueen(num + 1)
+        
+nqueen(0)
+print(total)
