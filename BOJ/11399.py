a79198be5b7b84abb8558ac28d3f94a2cdc98570
jim-min <102157329+jim-min@users.noreply.github.com>
2025-06-13 00:00:31 +0900

    Create 11399.py

---
+++ BOJ/11399.py
@@ -0,0 +1,12 @@
+ppl = int(input())
+
+costs = list(map(int, input().split()))
+costs.sort()
+
+ans = 0
+
+for idx, _ in enumerate(costs):
+    for j in range(idx + 1):
+        ans += costs[j]
+
+print(ans)
