90adf2c5445a1fda990e3329995ae29addc71885
jim-min <102157329+jim-min@users.noreply.github.com>
2025-06-21 00:08:07 +0900

    Create 30802.py

---
+++ BOJ/30802.py
@@ -0,0 +1,15 @@
+from math import ceil
+
+n = int(input())
+
+shirts = list(map(int, input().split()))
+t, p = map(int, input().split())
+
+result = 0
+
+for i in shirts:
+    result += ceil(i/t)
+
+print(result)
+
+print(n//p, n%p)
