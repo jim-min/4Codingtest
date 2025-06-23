becdde45690df89631209555f90cb80d547d06dd
jim-min <102157329+jim-min@users.noreply.github.com>
2025-06-23 11:39:29 +0900

    Create 1260.py

---
+++ BOJ/1260.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def dfs(v):
+    global visited
+    visited[v] = True
+    print(v, end=" ")
+
+    for i in graph[v]:
+        if not visited[i]:
+           dfs(i)
+
+def bfs(v):
+    global visited
+    queue = deque([v])
+    visited[v] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+
+visited = [False] * (n + 1)
+dfs(v)
+print()
+
+visited = [False] * (n + 1)
+bfs(v)
+print()
