761e6b5a8db3e069236e1f8442c2bf9b8e83e597
jim-min <102157329+jim-min@users.noreply.github.com>
2025-06-24 23:30:46 +0900

    Create 15650.py

---
+++ BOJ/15650.py
@@ -0,0 +1,14 @@
+def back(level, now):
+    arr[now - 1] = level
+    if now == m:
+        print(*arr)
+    
+    else:
+        for i in range(level + 1, n + 1):
+            back(i, now + 1)
+
+n, m = map(int, input().split())
+
+for i in range(1, n + 2 - m):
+    arr = [i] * m
+    back(i, 1)
