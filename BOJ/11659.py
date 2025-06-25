c38890b2dad69cf7abb1af4517b457410f705785
jim-min <102157329+jim-min@users.noreply.github.com>
2025-06-25 16:50:09 +0900

    Update 11659.py

--- BOJ/11659.py
+++ BOJ/11659.py
@@ -1,7 +1,21 @@
-n, m = map(int, input().split())
-nums = list(map(int, input().split()))
+import sys # stdin.readline 안 쓰면 시간 초과남 - PyPy는 되긴 함
+
+n, m = map(int, sys.stdin.readline().split())
+nums = list(map(int, sys.stdin.readline().split()))
+accumulated_sum = [0] * (n + 1)
+accumulated_sum[1] = nums[0]
+idx = 0
+
+for i in nums:
+    if idx == 0:
+        idx += 1
+        continue
+
+    accumulated_sum[idx + 1] = accumulated_sum[idx] + i
+    idx += 1
+
 
 for _ in range(m):
-    i, j = map(int, input().split())
+    i, j = map(int, sys.stdin.readline().split())
 
-    print(sum(nums[i-1:j]))
+    print(accumulated_sum[j] - accumulated_sum[i - 1])
