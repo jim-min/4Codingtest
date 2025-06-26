6e6e8b7c19f669af259ac39e030ab508ecc759f3
jim-min <102157329+jim-min@users.noreply.github.com>
2025-06-27 01:08:52 +0900

    Create 2805.py

---
+++ BOJ/2805.py
@@ -0,0 +1,23 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+trees = list(map(int, sys.stdin.readline().split()))
+
+lowest, highest = 0, max(trees) - 1
+
+while lowest <= highest:
+  mid = (lowest + highest) // 2
+
+  cut = 0
+
+  for i in trees:
+    if i > mid:
+      cut += i - mid
+
+  if cut >= m:
+    lowest = mid + 1
+
+  else:
+    highest = mid - 1
+
+print(highest)
