24981c0bf71ae41e769d64d7b7b6958732092e1e
jim-min <102157329+jim-min@users.noreply.github.com>
2025-07-02 00:44:03 +0900

    Create 6588.py

---
+++ BOJ/6588.py
@@ -0,0 +1,32 @@
+import sys
+
+lim = 1000000
+c = [False, False] + [True] * (lim - 1)
+primes = []
+
+for i in range(2, lim//2 + 1):
+    if c[i] == False:
+        continue
+
+    primes.append(i)
+
+    for j in range(2*i, lim + 1, i):
+        c[j] = False
+
+n = int(sys.stdin.readline())
+
+while (n != 0):
+    if (n % 2 == 1):
+        print("Goldbach's conjecture is wrong.")
+        n = int(sys.stdin.readline())
+        continue
+        
+    for k in primes:
+        first = k
+        second = n - first
+
+        if c[second]:
+            print(n, "=", first, "+", second)
+            break
+
+    n = int(sys.stdin.readline())
