727237a16c529e73cdf95193f93053f7afbc9971
jim-min <102157329+jim-min@users.noreply.github.com>
2025-07-16 00:01:38 +0900

    Update 15654.py

--- BOJ/15654.py
+++ BOJ/15654.py
@@ -1,21 +1,42 @@
-def dfs(depth):
-    if depth==m:
-        print(' '.join(map(str, result)))
+# def dfs(depth):
+#     if depth==m:
+#         print(' '.join(map(str, result)))
         
-    for i in range(n):
-        if not vist[i]:
-            result.append(numList[i])
-            vist[i] = True
-            dfs(depth + 1)
-            result.pop()
-            vist[i] = False
+#     for i in range(n):
+#         if not vist[i]:
+#             result.append(numList[i])
+#             vist[i] = True
+#             dfs(depth + 1)
+#             result.pop()
+#             vist[i] = False
+
+# n, m = map(int, input().split())
+# numList = list(map(int, input().split()))
+
+# numList.sort()
+
+# vist=[False]*n
+# result = []
+
+# dfs(0)
+
+# 이걸 내가 언제 풀었는지 모르겠음
+
+def solve(i, lvl):
+    global ans
+
+    if (lvl == m):
+        print(*ans)
+
+    else:
+        for j in range(n):
+            if nums[j] not in ans[0:lvl]:
+                ans[lvl] = nums[j]
+                solve(j + 1, lvl + 1)
 
 n, m = map(int, input().split())
-numList = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+nums.sort()
 
-numList.sort()
-
-vist=[False]*n
-result = []
-
-dfs(0)
+ans = [-1] * m
+solve(0, 0)
