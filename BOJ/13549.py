dfea617fbf044e107b82031a02e1dd116ad39eee
jim-min <102157329+jim-min@users.noreply.github.com>
2025-07-16 15:32:58 +0900

    Create 13549.py

---
+++ BOJ/13549.py
@@ -0,0 +1,32 @@
+# 골드 5, 0-1 BFS
+
+from collections import deque
+
+n, k = map(int, input().split())
+
+max = 100000
+time = [max] * (max + 1)
+
+graph = deque()
+graph.append(n)
+time[n] = 0
+
+while (graph):
+    a = graph.popleft()
+
+    if a == k:
+        break
+
+    if a*2 <= max and time[a*2] == max:
+        graph.appendleft(a*2)
+        time[a*2] = time[a]
+
+    if (a-1) >= 0 and time[a-1] == max:
+        graph.append(a-1)
+        time[a-1] = time[a] + 1
+
+    if (a+1) <= max and time[a+1] == max:
+        graph.append(a+1)
+        time[a + 1] = time[a] + 1
+
+print(time[k])
