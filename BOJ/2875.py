fc902b0d62883b4b9885dc24a17cc720b639e06d
jim-min <102157329+jim-min@users.noreply.github.com>
2025-07-31 14:39:38 +0900

    [Bronze III}

---
+++ BOJ/2875.py
@@ -0,0 +1,20 @@
+n, m, k = map(int, input().split())
+team = 0
+while (n>1 and m>0):
+    n -= 2
+    m -= 1
+    team += 1
+
+while (n>0 and k>0):
+    n -= 1
+    k -= 1
+
+while (m>0 and k>0):
+    m -= 1
+    k -= 1
+
+if (k>0):
+    print(team - (k+2)//3)
+
+else:
+    print(team)
