06554cc4ba6c5595a58de3abbc95d94245e98be1
jim-min <102157329+jim-min@users.noreply.github.com>
2025-08-20 17:05:30 +0900

    [Gold V, Knapsack] 평범한 배낭

---
+++ BOJ/12865.py
@@ -0,0 +1,18 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+items = []
+
+for _ in range(n):
+    w, v = map(int, sys.stdin.readline().split())
+
+    items.append([w, v])
+
+dp = [0] * (k + 1)
+
+for w, v in items:
+    for i in range(k, w-1, -1):
+        if dp[i] < dp[i-w] + v:
+            dp[i] = dp[i-w] + v
+
+print(dp[k])
